8094469bc1f3ba681f755ef8204e8c1161d9dc31
Luca Carubia <luca.carubia42@gmail.com>
2020-01-09 17:08:30 +0100

    search by searchbar functionnal

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\SearchProductsType;
 use App\Service\Coach2StoreApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,18 +17,34 @@ class CatalogController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param Coach2StoreApi $apiService
+     * @param Request $request
      * @return Response
      */
-    public function index(Coach2StoreApi $apiService): Response
+    public function index(Coach2StoreApi $apiService, Request $request): Response
     {
-        $productsByBrand = $apiService->getProductsByBrand('vélo');
-        $productsBySupplier = $apiService->getProductsBySupplier('tennis');
-        $productsTop = $apiService->getProductsTop();
+        $brands = [];
+        $suppliers = [];
+        $products = $apiService->getProductsTop();
+        $form = $this->createForm(SearchProductsType::class, null, [
+            'method' => 'GET',
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $criteria = $data['search'];
+            $result = $apiService->getProductsByCriteria($criteria);
+            $products = $result['hits']['hit'];
+            $products = array_map('App\Service\Coach2StoreApi::simplify', $products);
+            $brands = $result['facets']['brand']['buckets'];
+            $suppliers = $result['facets']['supplier_name']['buckets'];
+        }
 
         return $this->render('catalog/index.html.twig', [
-            'productsByBrand' => $productsByBrand,
-            'productsBySupplier' => $productsBySupplier,
-            'productsTop' => $productsTop,
+            'brands' => $brands,
+            'suppliers' => $suppliers,
+            'productsTop' => $products,
+            'form' => $form->createView(),
         ]);
     }
 }
